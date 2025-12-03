6c37d0732d97fb3f735897013dd658408833d045
baoqk1online <baoqk1online@gmail.com>
2025-12-03 14:59:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let processedImages = [];
 
 // Load watermark mặc định từ assets/watermark.png
 let watermarkImage = new Image();
-watermarkImage.src = 'bqk1.png';
+watermarkImage.src = 'Asset 2.png';
 watermarkImage.onload = () => {
     log.innerHTML += '<p>Watermark mặc định đã sẵn sàng.</p>';
 };
